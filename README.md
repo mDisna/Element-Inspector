2cdd4c9bd1b495da10e0776e3d0fa4989884e175
mDisna <mdisna@gmail.com>
2021-02-15 18:18:20 -0500

    Updated to version 2.0

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Element Selector
+# Element Selector v2.0
 
 ## Project Description
 
-The purpose of this jQuery script is to allow users to inspect HTML elements without needing to fiddle with the browser Developer Tools and without needing to install an broswer plugins. When enabled, simply click on an element to find out it's classes, ID, and CSS Path. This information will be shown in a sticky banner at the top or bottom of the browser window.
+The purpose of this jQuery script is to allow users to inspect HTML elements without needing to fiddle with the browser Developer Tools and without needing to install an broswer plugins. When enabled, simply click on an element to find out it's classes, ID, and selector. This information will be shown in a sticky banner at the top or bottom of the browser window.
 
 ## Installation
 
 ### Script Setup
 
-This scritp requires jQuery - if you do not already have jQuery included on your website, add the following within you `<head></head>` tags:
+This script requires jQuery - if you do not already have jQuery included on your website, add the following within you `<head></head>` tags:
 
 `<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>`
 
@@ -18,45 +18,23 @@ Next, add the `element-inspector.js` file, updating the file path to where you h
 
 ### Using the Script
 
-To use the script, add the activation keyword to the end of the URL on the page you wish to use it on. The default activation keyword is `?dev`. This will load the page with a white banner across the bottom of the webpage. While activated all links and buttons will no longer function, and clicking on an element will display it's information in the banner. If the banner is blocking a element, add "-top" to the end of the activation keyword, for example: `?dev-top` to display the banner at the top of the browser.
+To use the script, add the activation keyword to the end of the URL on the page you wish to use it on. The default activation keyword is `?inspect`.
 
-To deactivate the script, simple remvove the keyword from the URL.
+While activated, links and buttons on the webpage will be disabled. Clicking on any element will bring up a modal window with the details about the selected element. Clicking on the displayed element tag will open a new window to the W3Schools.com entry for that element. To close the modal, click anywhere outside the modal.
+
+To deactivate the script, click on the link in the yellow banner or remove the activation keyword from the URL and reload thepage.
 
 ## Advanced
 
 ### Changing the Activation Keyword
 
-If you need to change the activation keyword, edit the `element-inspector.js` file and look for the following near the top of the file:
+If you need to change the activation keyword, you can add the following JavaScript to your project
 
 ```
+<script>
 // Sets the activation kewword to look for in the URL
-const ActivateKeyword = "?dev";
+const activateKeyword = "?newActivationCode";
+</scrit>
 ```
 
-Change the `ActivateKeyword` constant to something more fitting to your needs. Starting the keyword with a symbol such as "?" or "#" is recommended.
-
-### Changing the ID of the Details Banner
-
-If you need to change the ID used for the details banner, edit the `element-inspector.js` file and look for the following near the top of the file:
-
-```
-// Sets the ID for the details container
-const DetailsDivId = "el-details";
-```
-
-Change the `DetailsDivId` constand to something more fitting to your needs.
-
-### Changing the Initial Banner Content
-
-You can change the default text shown in the banner when it firsts loads. To do this, edit the `element-inspector.js` file and look for the following near the top of the file:
-
-```
-// Sets up the details container with initial message
-const DetailsDiv =  '<div id="' + DetailsDivId + '"><h1>Click on an element to begin</h1></div>';
-```
-
-Change the text `<h1>Click on an element to begin</h1>` to whatever you like. This can container HTML tags.
-
-### Changing the CSS of the Details Banner
-
-The CSS for the banner is contained within the `element-inspector.js` file. To change it, edit the file and look for a function called `setCSS` at the bottom of the file. Here you can adjust the CSS using JavaScript notation.
+Change the `?newActivationCode` constant to something more fitting to your needs. Starting the keyword with a symbol such as "?" or "#" is recommended.
--- element-inspector.js
+++ element-inspector.js
@@ -1,29 +1,32 @@
 /*
   Element Inspector (c) Matthieu Disna
-  V.1.0.0
+  V.2.0.0
 
   USAGE: Include jQuery and this file in your header.
   Add activation keyword to your URL to use.
-  Add "-top" after your activation keyword in URL to
-  show details continaer at top of window.
   Click on elements to find their CSS information.
 */
 
 /***** CONSTANTS & VARIABLES *****/
 
-// Sets the activation kewword to look for in the URL
-const ActivateKeyword = "?dev";
-// Sets the ID for the details container
-const DetailsDivId = "el-details";
-// Sets up the details container with initial message
-const DetailsDiv =
-  '<div id="' + DetailsDivId + '"><h1>Click on an element to begin</h1></div>';
+// Sets the activation keyword to look for in the URL
+if (typeof activateKeyword === "undefined") {
+  activateKeyword = "?inspect";
+}
+
+const inspectorRunningDiv =
+  '<div id="inspectorRunning"><strong>The Element Inspector is running.</strong> <a href="' +
+  window.location.href.replace(activateKeyword, "") +
+  '">Click here to exit</a>.</div>';
+
+const inspectorModal =
+  '<div id="inspectorModalBackgroundHolder"><div id="inspectorModalBackground"></div><div id="inspectorModalContent"><div id="inspectorModalDetails"></div></div></div>';
 
 /***** START *****/
 
 $(document).ready(function () {
   // Check for activation keyword in URL
-  if (window.location.href.indexOf(ActivateKeyword) > -1) {
+  if (window.location.href.indexOf(activateKeyword) > -1) {
     startDev();
   }
 });
@@ -32,37 +35,90 @@ $(document).ready(function () {
 
 startDev = () => {
   // Add details div to page body
-  $("body").prepend(DetailsDiv);
-  // Add CSS to created div
+  $("body").prepend(inspectorRunningDiv);
+  $("body").prepend(inspectorModal);
+
+  // Add CSS
   setCSS();
 
-  // INtercept all page clicks
+  // Cancel all onclicks
+  $("*").attr("onclick", "").unbind("click");
+  $("tr").attr("onclick", "").unbind("click");
+
+  // Intercept all page clicks
   $("body").on("click", "*", function (event) {
+    // Use only the topmost element
+    event.stopPropagation();
+
     // Store clicked element ID
     let clickedID = $(this).attr("id");
     // Store clicked element class names
     let clickedClass = $(this).attr("class");
 
+    // Store clicked element type
+    let clickedType = $(this).prop("nodeName");
+
     // Obtain CSS path of clicked element
     completePath = getPath(this);
 
-    // Ensure clicked element is not details container
-    if (completePath.indexOf("#" + DetailsDivId) == -1) {
-      // Cancel default action for clicked element
-      event.preventDefault();
+    // Ensure clicked element is not inspector container
+    if (
+      completePath.indexOf("#inspectorRunning") == -1 &&
+      completePath.indexOf("#inspectorModalContent") == -1
+    ) {
       // Show element information
-      displyPath(completePath, clickedID, clickedClass);
-    }
+      displyPath(completePath, clickedID, clickedClass, clickedType);
 
-    // This prevents the function from firing multiple times for nested elements
-    return false;
+      // This prevents the function from firing multiple times for nested elements
+      return false;
+    }
   });
 };
 
-/***** CSS PATH FUNCTION *****/
-// FROM: https://stackoverflow.com/questions/3620116/get-css-path-from-dom-element
+function displyPath(cssPath, elID, elClass, elType) {
+  if (/(h[1-9])/i.test(elType)) {
+    elLink = "hn";
+  } else {
+    elLink = elType;
+  }
+  elInfo =
+    "<h3>Selected Element: <a id='inspectorElLink' target='_blank' href='https://www.w3schools.com/tags/tag_" +
+    elLink +
+    ".asp'>&#12296;" +
+    elType +
+    "&#12297;</a></h3>";
+  if (cssPath.lastIndexOf("#") > -1) {
+    // If the path contains an ID, start path from there
+    cssPath = cssPath.substring(cssPath.lastIndexOf("#"));
+  }
+  if (elID) {
+    // If clicked element has ID, display it
+    elInfo = elInfo + "<p><b>Element ID:</b> #" + elID + "</p>";
+  }
+  if (elClass) {
+    // If clicked element has classes, display them
+    elInfo =
+      elInfo +
+      "<p><b>Element Class(es):</b> ." +
+      elClass.split(" ").join(" .") +
+      "</p>";
+  }
+  // Display CSS path
+  elInfo = elInfo + "<p><b>Selector Path:</b> " + cssPath + "</p>";
+  console.log(elInfo);
+  // Display modal
+  $("#inspectorModalContent").html(elInfo);
+  $("#inspectorModalContent,#inspectorModalBackground").toggleClass("active");
+  setCSS();
+  // Hide modal
+  $("#inspectorModalBackground").click(function () {
+    $("#inspectorModalContent,#inspectorModalBackground").toggleClass("active");
+    setCSS();
+  });
+}
 
-getPath = (el) => {
+// Get full CSS path
+function getPath(el) {
   let path = [];
   while (el.nodeType === Node.ELEMENT_NODE) {
     let selector = el.nodeName.toLowerCase();
@@ -81,54 +137,66 @@ getPath = (el) => {
     path.unshift(selector);
     el = el.parentNode;
   }
-  return path.join(" > ");
-};
 
-/***** FORMAT & DISPLAY DETAILS FUNCTION *****/
-
-displyPath = (cssPath, elID, elClass) => {
-  elInfo = "";
-  if (cssPath.indexOf("#") > -1) {
-    // If the path contains an ID, start path from the last one
-    cssPath = cssPath.substring(cssPath.lastIndexOf("#"));
-  }
-  if (elID) {
-    // If clicked element has ID, display it
-    elInfo = elInfo + "<p><b>Element ID:</b> #" + elID + "</p>";
-  }
-  if (elClass) {
-    // If clicked element has classes, display them
-    elInfo =
-      elInfo +
-      "<p><b>Element Class(es):</b> ." +
-      elClass.split(" ").join(" .") +
-      "</p>";
-  }
-  // Display CSS path
-  elInfo = elInfo + "<p><b>CSS Path:</b> " + cssPath + "</p>";
-  // Add to details container
-  $("#" + DetailsDivId).html(elInfo);
-};
+  elementPath = path.join(" > ");
+  elementPath = elementPath.substring(elementPath.lastIndexOf("body:"));
+  return elementPath;
+}
 
 /***** CSS Styling *****/
 // Styles done via JS to avoid needing multiple files
 setCSS = () => {
-  $("#" + DetailsDivId).css({
-    border: "5px solid #000",
-    width: "100%",
-    background: "#fff",
-    color: "#000",
-    position: "fixed",
-    padding: "20px",
-    "z-index": 999,
+  $("#inspectorRunning").css({
+    color: "#8a6d3b",
+    backgroundColor: "#fcf8e3",
+    borderColor: "#faebcc",
+    padding: "15px",
+    marginBottom: "20px",
+    border: "1px solid transparent",
+    borderRadius: "4px",
+    textAlign: "center",
+    position: "sticky",
+    top: 0,
+    zIndex: 99999,
+  });
+  $("#inspectorRunning a").css({
+    color: "#66512c",
+    textDecoration: "none",
   });
 
-  // Show details container at top or bottom of viewport based on activation keyword
-  if (window.location.href.indexOf(ActivateKeyword + "-top") > -1) {
-    $("#" + DetailsDivId).css({ top: 0 });
-    $("body").css({ "padding-top": "150px" });
-  } else {
-    $("#" + DetailsDivId).css({ bottom: 0 });
-    $("body").css({ "padding-bottom": "150px" });
-  }
+  $("#inspectorModalBackground").css({
+    display: "none",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+    opacity: ".50",
+    filter: "alpha(opacity=50)",
+    zIndex: 99990,
+  });
+
+  $("#inspectorModalContent").css({
+    backgroundColor: "white",
+    borderRadius: "10px",
+    boxShadow: "0 0 20px 0 #222",
+    display: "none",
+    height: "240px",
+    left: "50%",
+    margin: "-120px 0 0 -300px",
+    padding: "10px",
+    position: "fixed",
+    top: "50%",
+    width: "600px",
+    zIndex: 99995,
+  });
+
+  $("#inspectorModalContent a").css({
+    textDecoration: "none",
+  });
+
+  $("#inspectorModalBackground.active, #inspectorModalContent.active").css({
+    display: "block",
+  });
 };
